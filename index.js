4696402007a59b4b207aeea2bd93af3a222a8347
Anthony White <awhite@dataxu.com>
2015-03-17 14:50:15 -0400

    fix: Strip down compilation, use typescript-compiler

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
 	path = require('path'),
 	_ = require('lodash'),
-	compile = require('node-tsc').compile;
+  ts = require('typescript-compiler');
 
 var createTypeScriptPreprocessor = function(args, config, logger, helper) {
 	config = config || {};
@@ -68,39 +68,9 @@ function tsc(file, content, options, callback, log) {
 	file.outputPath = output + '.ktp.js';
 	file.sourceMapPath = output + '.map';
 
-	if (!('module' in args)) {
-		args.out = output;
-	}
-
-	var opts = {files: [input], args: args};
-
-	fs.writeFileSync(input, content);
-
-	if (options.compiler) {
-		delete args.compiler;
-		opts.compiler = options.compiler;
-	}
-
-	compile(opts, function(err) {
-		log.error(err);
-	});
-
-	fs.unlinkSync(input);
-
-	if (fs.existsSync(file.path)) {
-		fs.unlinkSync(file.path);
-	}
-
 	log.debug('preprocessed "%s"', file.originalPath);
 
-	fs.readFile(output, 'utf8', function(error, content) {
-		if (error) {
-			callback(error, null);
-			return;
-		}
-		fs.unlinkSync(output);
-		callback(null, content);
-	});
+  callback(null, ts.compileString(content));
 }
 
 createTypeScriptPreprocessor.$inject = ['args', 'config.typescriptPreprocessor', 'logger', 'helper'];
